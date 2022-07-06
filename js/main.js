bad76cdce2fb465fd0baba125956f499aa314bc1
wwdk <61912053+wdkkk@users.noreply.github.com>
2022-07-06 15:06:42 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const menu = document.getElementById('menu');
 const nav_text = document.getElementById('nav_text_row');
+const body = document.body;
+
 menu.onclick = function () {
     menu.classList.toggle('__active');
     nav_text.classList.toggle('__active');
+    document.body.classList.toggle('fixed');
 }
 
 window.addEventListener("resize", displayWindowSize);
